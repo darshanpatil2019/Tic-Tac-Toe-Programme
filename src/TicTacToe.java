40db5c67110fced016cf7beed8bd92e77263e766
Darshan Patil <iampatildarsh>
2022-12-16 14:49:59 +0530

    User can choose valid cell to make his move

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,7 +8,7 @@ public class TicTacToe {
     private static char player2Choice;
 
     public static void initiateBoard() { //board initialization - uc1
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 9; i++) {
             boardArray[i] = ' ';
         }
     }
@@ -30,13 +30,13 @@ public class TicTacToe {
     }
 
     public static void showBoard() {  // ShowBoard method to display the current board
-        System.out.println(" ----------- ");
+
         System.out.println("| " + boardArray[1] + " | " + boardArray[2] + " | " + boardArray[3] + " |");
         System.out.println("|-----------|");
         System.out.println("| " + boardArray[4] + " | " + boardArray[5] + " | " + boardArray[6] + " |");
         System.out.println("|-----------|");
         System.out.println("| " + boardArray[7] + " | " + boardArray[8] + " | " + boardArray[9] + " |");
-        System.out.println(" ----------- ");
+
     }
 
     public static void main(String[] args) {
